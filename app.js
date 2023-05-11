21a9dbb8b9b8aec659f0eb9be8bed66ab38a09b0
bill <bill255203@gmail.com>
2023-05-11 21:56:17 +0800

    2023/5/11

--- app.js
+++ app.js
@@ -3,6 +3,48 @@ const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
+const images = document.querySelectorAll('img');
+
+//images iteration
+const imageFiles = ['bill1.png', 'bill2.png'];
+const slideshow = document.getElementById('slideshow');
+let currentIndex = 0;
+const image = document.createElement('img');
+image.setAttribute('src', `img/${imageFiles[0]}`);
+slideshow.appendChild(image);
+currentIndex = 1;
+setTimeout(() => {
+  image.style.filter = 'grayscale(0%)';
+}, 500);
+setTimeout(() => {
+  image.style.filter = 'grayscale(100%)';
+}, 2500);
+setTimeout(() => {
+  slideshow.removeChild(image);
+}, 3000);
+
+const interval = setInterval(() => {
+  const image = document.createElement('img');
+  image.setAttribute('src', `img/${imageFiles[currentIndex]}`);
+  slideshow.appendChild(image);
+  currentIndex++;
+  if (currentIndex >= imageFiles.length) {
+    currentIndex = 0;
+  }
+  setTimeout(() => {
+    image.style.filter = 'grayscale(0%)';
+  }, 200);
+
+  // set grayscale filter after a delay
+  setTimeout(() => {
+    image.style.filter = 'grayscale(100%)';
+  }, 2800);
+
+  setTimeout(() => {
+    slideshow.removeChild(image);
+  }, 3000);
+}, 3000);
+
 function PageTransitions() {
   //button click active class
   for (let i = 0; i < sectBtn.length; i++) {
